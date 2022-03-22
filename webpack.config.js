739f6f9e38d4f62c8af605c72f46ed564dd8639f
raoakif <61361037+RaoAkif@users.noreply.github.com>
2022-03-23 03:22:04 +0500

    Follow the tutorial

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+  entry: {
+    index: path.resolve(__dirname, 'src', 'index.js'),
   },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack Starter',
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
 };
